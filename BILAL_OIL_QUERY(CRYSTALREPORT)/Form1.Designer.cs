98aa44862b0238e6fc5e1429e8c6c95e23a929f9
Muhammad Saad <muhammadsaad3119@gmail.com>
2021-12-26 02:36:45 +0500

    Add project files.

---
+++ BILAL_OIL_QUERY(CRYSTALREPORT)/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace BILAL_OIL_QUERY_CRYSTALREPORT_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(988, 429);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Details:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Starting Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(636, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Ending Date:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "View Full Report",
+            "OLIEN",
+            "RBD",
+            "SARSON",
+            "CP-6",
+            "FATTY",
+            "CANOLLA"});
+            this.comboBox1.Location = new System.Drawing.Point(258, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(503, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(122, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(739, 27);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(122, 20);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.Location = new System.Drawing.Point(877, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 22);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleGreen;
+            this.button2.Location = new System.Drawing.Point(621, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 22);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "View Full Report";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 429);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form1";
+            this.Text = "Bilal-Oil-Report";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BILAL_OIL_QUERY(CRYSTALREPORT)/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace BILAL_OIL_QUERY_CRYSTALREPORT_
+{
+    public partial class Form1 : Form
+    {
+        ReportDocument crypt = new ReportDocument();
+        SqlConnection con = new SqlConnection("Data Source=51.89.37.225;Initial Catalog=BILAL_OIL;User ID=sa;Password=blue@1122");
+        SqlDataAdapter sda;
+                                                                                                                        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text=="View Full Report")
+            {
+            
+            con.Open();
+            sda = new SqlDataAdapter("select materials.material_id,materials.material_name,purchases_order.date,purchases_order.po_number,purchases_order.weight from materials inner join purchases_order on materials.material_id =purchases_order.material_id where date between '"+this.dateTimePicker1.Value.ToString("yyyyMMdd") + "'and '"+this.dateTimePicker2.Value.ToString("yyyyMMdd") + "'",con);
+
+            DataSet1 ds = new DataSet1();
+            sda.Fill(ds, "Materials_Detail");
+            crypt.Load("CrystalReport1.rpt");
+            con.Close();
+            crypt.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = crypt;
+
+            }
+            else
+            { 
+            con.Open();
+            sda = new SqlDataAdapter("select materials.material_id, materials.material_name, purchases_order.date, purchases_order.po_number, purchases_order.weight from materials inner join purchases_order on materials.material_id = purchases_order.material_id where materials.material_name = '" + comboBox1.Text + "' and date between'" + this.dateTimePicker1.Value.ToString("yyyyMMdd") + "'and '" + this.dateTimePicker2.Value.ToString("yyyyMMdd") + "'", con);
+
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "Materials_Detail");
+            crypt.Load("CrystalReport1.rpt");
+            crypt.SetDataSource(ds);
+            con.Close();
+            crystalReportViewer1.ReportSource = crypt;
+            crystalReportViewer1.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            sda = new SqlDataAdapter("select materials.material_id,materials.material_name,purchases_order.date,purchases_order.po_number,purchases_order.weight from materials inner join purchases_order on materials.material_id =purchases_order.material_id", con);
+
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "Materials_Detail");
+            crypt.Load("CrystalReport1.rpt");
+            con.Close();
+            crypt.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = crypt;
+        }
+    }
+}
